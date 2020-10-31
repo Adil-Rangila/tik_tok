6c35f837ae27f4d464f1cf30d41aa24a93231d53
Adil Ahmed <48706857+Adil-Rangila@users.noreply.github.com>
2020-10-31 16:24:45 +0500

    video page work is started

--- lib/pages/videos.dart
+++ lib/pages/videos.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tik_tok/variables.dart';
 
 class Videos extends StatefulWidget {
   @override
@@ -8,6 +9,36 @@ class Videos extends StatefulWidget {
 class _VideosState extends State<Videos> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Stack(
+        children: [
+          //video
+          Container(
+            width: size.width,
+            height: size.height,
+            decoration: BoxDecoration(color: Colors.black),
+          ),
+          Column(
+            children: [
+              //top section
+              Row(
+                children: [
+                  Text(
+                    'Followers',
+                    style: myStyle(17, Colors.white),
+                  ),
+                  SizedBox(width: 15),
+                  Text(
+                    'Following',
+                    style: myStyle(17, Colors.white),
+                  )
+                ],
+              )
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
