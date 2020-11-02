14b0b942262cef14d3f1fc285cb2f1419d5527b9
Adil Ahmed <48706857+Adil-Rangila@users.noreply.github.com>
2020-11-02 12:42:20 +0500

    updated

--- lib/pages/videos.dart
+++ lib/pages/videos.dart
@@ -7,6 +7,22 @@ class Videos extends StatefulWidget {
 }
 
 class _VideosState extends State<Videos> {
+  buildProfile() {
+    return Container(
+      width: 60,
+      height: 60,
+      child: Stack(
+        children: [
+          Container(
+            child: ClipRRect(
+              child: Image.network(''),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
@@ -22,19 +38,71 @@ class _VideosState extends State<Videos> {
           Column(
             children: [
               //top section
-              Row(
-                children: [
-                  Text(
-                    'Followers',
-                    style: myStyle(17, Colors.white),
+              Container(
+                color: Colors.red,
+                height: 100,
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text(
+                      'Following',
+                      style: myStyle(17, Colors.white),
+                    ),
+                    SizedBox(width: 15),
+                    Text(
+                      'Followers',
+                      style: myStyle(17, Colors.white),
+                    )
+                  ],
+                ),
+              ),
+              //middle Section
+              Expanded(
+                child: Container(
+                  padding: EdgeInsets.only(left: 20, bottom: 10),
+                  color: Colors.orange,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Text(
+                        'Adil',
+                        style: myStyle(20, Colors.white),
+                      ),
+                      Text(
+                        'Cool Video keep up the good work',
+                        style: myStyle(15, Colors.white),
+                      ),
+                      Row(
+                        children: [
+                          Icon(
+                            Icons.music_note_rounded,
+                            color: Colors.white,
+                          ),
+                          Text(
+                            'Enriq Iqlius',
+                            style: myStyle(
+                              15,
+                              Colors.white,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
                   ),
-                  SizedBox(width: 15),
-                  Text(
-                    'Following',
-                    style: myStyle(17, Colors.white),
-                  )
-                ],
-              )
+                ),
+              ),
+              Container(
+                color: Colors.blue,
+                width: 100,
+                //  height: 100,
+                child: Column(
+                  children: [
+                    buildProfile(),
+                  ],
+                ),
+              ),
             ],
           )
         ],
