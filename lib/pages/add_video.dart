25ad8a920df3b7dcf47518a8d19ace669ee08430
Adil Ahmed <48706857+Adil-Rangila@users.noreply.github.com>
2020-11-06 15:25:47 +0500

    vidwo is seletig

--- lib/pages/add_video.dart
+++ lib/pages/add_video.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:tik_tok/variables.dart';
 
 class AddVideo extends StatefulWidget {
   @override
@@ -6,8 +8,61 @@ class AddVideo extends StatefulWidget {
 }
 
 class _AddVideoState extends State<AddVideo> {
+  showUploadOptions() {
+    return showDialog(
+        context: context,
+        builder: (context) {
+          return SimpleDialog(
+            children: [
+              SimpleDialogOption(
+                onPressed: () {
+                  Navigator.pop(context);
+                  pickVideo(ImageSource.camera);
+                },
+                child: Text('Camra', style: myStyle(20)),
+              ),
+              SimpleDialogOption(
+                onPressed: () {
+                  Navigator.pop(context);
+                  pickVideo(ImageSource.gallery);
+                },
+                child: Text('Gallery', style: myStyle(20)),
+              ),
+              SimpleDialogOption(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Text('Cancil', style: myStyle(20)),
+              ),
+            ],
+          );
+        });
+  }
+
+  pickVideo(ImageSource src) async {
+    //Navigator.pop(context);
+    final video = await ImagePicker().getVideo(source: src);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: InkWell(
+        onTap: () => showUploadOptions(),
+        child: Center(
+          child: Container(
+            height: 90,
+            width: 190,
+            color: Colors.red[200],
+            child: Center(
+              child: Text(
+                'Add Video',
+                style: myStyle(30),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
